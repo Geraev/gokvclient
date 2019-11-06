6f533ccdb911643502963362242d3893a3868ea3
Geraev Rajiv <gheraev.rajiv@gmail.com>
2019-11-06 05:24:20 +0300

    Client complite

---
+++ handlers/cmdhandlers.go
@@ -0,0 +1,155 @@
+package handlers
+
+import (
+	"fmt"
+	"github.com/fatih/color"
+	"time"
+
+	"github.com/abiosoft/ishell"
+	"github.com/go-resty/resty/v2"
+)
+
+type CacheClient struct {
+	host     string
+	username string
+	password string
+	client   *resty.Client
+}
+
+var (
+	cyan = color.New(color.FgCyan).SprintFunc()
+	boldRed = color.New(color.FgRed, color.Bold).SprintFunc()
+)
+func NewCacheClient(host, username, password string) *CacheClient {
+	client := resty.New().
+		SetDisableWarn(true).
+		SetRetryCount(3).
+		SetRetryWaitTime(time.Second)
+
+	return &CacheClient{
+		host:     host,
+		username: username,
+		password: password,
+		client:   client,
+	}
+}
+
+func (c *CacheClient) getClient() *resty.Client {
+	return c.client.
+		SetHostURL(c.host).
+		SetBasicAuth(c.username, c.password)
+}
+
+func (c *CacheClient) SetLogin() func(c *ishell.Context) {
+	return func(ctx *ishell.Context) {
+		ctx.ShowPrompt(false)
+		defer ctx.ShowPrompt(true)
+
+		ctx.Println("Let's login")
+		ctx.Print("Username: ")
+		c.username = ctx.ReadLine()
+		ctx.Print("Password: ")
+		c.password = ctx.ReadPassword()
+	}
+}
+
+
+func (c *CacheClient) SetHost() func(c *ishell.Context) {
+	return func(ctx *ishell.Context) {
+		ctx.Println("Enter hostname and port")
+		ctx.Print("Host: ")
+		host := ctx.ReadLine()
+		ctx.Print("Port: ")
+		port := ctx.ReadLine()
+		c.host = "http://" + host + ":" + port
+		ctx.Println(cyan("Hostname set to", c.host))
+	}
+}
+
+func (c *CacheClient) Keys() func(c *ishell.Context) {
+	return func(ctx *ishell.Context) {
+		req := c.getClient().NewRequest()
+		resp, err := req.Get("cache/keys")
+		if err != nil {
+			ctx.Println(boldRed("error: ", err))
+		}
+		ctx.Println(cyan(string(resp.Body())))
+	}
+}
+
+func (c *CacheClient) Key() func(c *ishell.Context) {
+	return func(ctx *ishell.Context) {
+		req := c.getClient().NewRequest()
+		var (
+			resp *resty.Response
+			err error
+			key, internalKey string
+		)
+		switch len(ctx.Args) {
+		case 1:
+			key = ctx.Args[0]
+			resp, err = req.Get(fmt.Sprintf("cache/key/%s", key))
+		case 2:
+			internalKey = ctx.Args[1]
+			resp, err = req.Get(fmt.Sprintf("cache/key/%s/%s", key, internalKey))
+		default:
+			ctx.Println(boldRed("too many arguments"))
+			return
+		}
+		if err != nil {
+			ctx.Println(boldRed("error: ", err))
+		}
+		ctx.Println(cyan(string(resp.Body())))
+	}
+}
+
+func (c *CacheClient) Set() func(c *ishell.Context) {
+	return func(ctx *ishell.Context) {
+		req := c.getClient().NewRequest()
+		var (
+			resp *resty.Response
+			err error
+			vartype, key, value string
+		)
+		if len(ctx.Args) != 3 {
+			ctx.Println(boldRed("arguments error"))
+			return
+		}
+
+		vartype = ctx.Args[0]
+		key = ctx.Args[1]
+		value = ctx.Args[2]
+
+		switch vartype {
+		case "string", "list", "dictionary":
+			req.SetBody(value).
+			SetHeader("Content-Type", "application/json")
+			resp, err = req.Put(fmt.Sprintf("cache/set/%s/%s", vartype, key))
+		default:
+			ctx.Println(boldRed("type error"))
+			return
+		}
+		if err != nil {
+			ctx.Println(boldRed("error: ", err))
+		}
+		ctx.Println(cyan(string(resp.Body())))
+	}
+}
+
+func (c *CacheClient) Remove() func(c *ishell.Context) {
+	return func(ctx *ishell.Context) {
+		req := c.getClient().NewRequest()
+		var (
+			resp *resty.Response
+			err error
+			key string
+		)
+		key = ctx.Args[0]
+		resp, err = req.Delete(fmt.Sprintf("cache/remove/%s", key))
+		if err != nil {
+			ctx.Println(boldRed("error: ", err))
+		}
+		ctx.Println(cyan(string(resp.Body())))
+	}
+}
+
--- main.go
+++ main.go
@@ -1,27 +1,25 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"os"
-	"strings"
-	"time"
+
+	"github.com/geraev/gokvclient/handlers"
 
 	"github.com/abiosoft/ishell"
-	"github.com/fatih/color"
 	"github.com/jessevdk/go-flags"
 )
 
 type options struct {
 	Help     bool   `short:"h" long:"help" description:"show help message"`
-	Host     string `short:"s" long:"host" description:"host name for GoCache server" default:"localhost:8081"`
+	Host     string `short:"s" long:"host" description:"host name for GoCache server" default:"http://localhost:8081"`
 	Username string `short:"u" long:"username" description:"username"`
 	Password string `short:"p" long:"password" description:"password"`
 }
 
 var (
 	opts        options
-	cacheClient *CacheClient
+	cacheClient *handlers.CacheClient
 )
 
 func main() {
@@ -36,180 +34,61 @@ func main() {
 		os.Exit(0)
 	}
 
-	cacheClient = NewCacheClient(opts.Host, opts.Username, opts.Password)
+	cacheClient = handlers.NewCacheClient(opts.Host, opts.Username, opts.Password)
 
 	shell := ishell.New()
 	shell.Println("GoCache interactive client")
 
-	// handle login.
+	// Set login
 	shell.AddCmd(&ishell.Cmd{
 		Name: "login",
-		Help: "setting username and password for http basic authentication on endpoint",
+		Help: "set username and password for http basic authentication on endpoint",
 		Func: cacheClient.SetLogin(),
 	})
 
+	// Set host
 	shell.AddCmd(&ishell.Cmd{
 		Name: "host",
-		Help: "setting hostname and port",
+		Help: "set hostname and port",
 		Func: cacheClient.SetHost(),
 	})
 
+	// Get all keys
+	// Example:
+	// >>> keys
 	shell.AddCmd(&ishell.Cmd{
 		Name: "keys",
 		Help: "get all keys in cache",
 		Func: cacheClient.Keys(),
 	})
 
-	// handle "greet".
+	// Get value for key (and internal key)
+	// Example:
+	// >>> key <key>
 	shell.AddCmd(&ishell.Cmd{
-		Name:    "greet",
-		Aliases: []string{"hello", "welcome"},
-		Help:    "greet user",
-		Func: func(c *ishell.Context) {
-			name := "Stranger"
-			if len(c.Args) > 0 {
-				name = strings.Join(c.Args, " ")
-			}
-			c.Println("Hello", name)
-		},
+		Name: "key",
+		Help: "get value for key (and internal key)",
+		Func: cacheClient.Key(),
 	})
 
-
-	// multiple choice
+	// Set or update value
+	// Examples:
+	// >>> set string new_key '{"value": "string_value", "ttl": 10000}'
+	// >>> set string planets '{"value": ["earth","jupiter","saturn"], "ttl": 10000}'
+	// >>> set string planets_map '{"value": ["earth":2220,"jupiter":3899,"saturn":23000], "ttl": 10000}'
 	shell.AddCmd(&ishell.Cmd{
-		Name: "choice",
-		Help: "multiple choice prompt",
-		Func: func(c *ishell.Context) {
-			choice := c.MultiChoice([]string{
-				"Golangers",
-				"Go programmers",
-				"Gophers",
-				"Goers",
-			}, "What are Go programmers called ?")
-			if choice == 2 {
-				c.Println("You got it!")
-			} else {
-				c.Println("Sorry, you're wrong.")
-			}
-		},
+		Name: "set",
+		Help: "set or update value",
+		Func: cacheClient.Set(),
 	})
 
-	// multiple choice
+	// Remove key
+	// Examples:
+	// >>> remove <key>
 	shell.AddCmd(&ishell.Cmd{
-		Name: "checklist",
-		Help: "checklist prompt",
-		Func: func(c *ishell.Context) {
-			languages := []string{"Python", "Go", "Haskell", "Rust"}
-			choices := c.Checklist(languages,
-				"What are your favourite programming languages ?",
-				nil)
-			out := func() (c []string) {
-				for _, v := range choices {
-					c = append(c, languages[v])
-				}
-				return
-			}
-			c.Println("Your choices are", strings.Join(out(), ", "))
-		},
-	})
-
-	// progress bars
-	{
-		// determinate
-		shell.AddCmd(&ishell.Cmd{
-			Name: "det",
-			Help: "determinate progress bar",
-			Func: func(c *ishell.Context) {
-				c.ProgressBar().Start()
-				for i := 0; i < 101; i++ {
-					c.ProgressBar().Suffix(fmt.Sprint(" ", i, "%"))
-					c.ProgressBar().Progress(i)
-					time.Sleep(time.Millisecond * 100)
-				}
-				c.ProgressBar().Stop()
-			},
-		})
-
-		// indeterminate
-		shell.AddCmd(&ishell.Cmd{
-			Name: "ind",
-			Help: "indeterminate progress bar",
-			Func: func(c *ishell.Context) {
-				c.ProgressBar().Indeterminate(true)
-				c.ProgressBar().Start()
-				time.Sleep(time.Second * 10)
-				c.ProgressBar().Stop()
-			},
-		})
-	}
-
-	// subcommands and custom autocomplete.
-	{
-		var words []string
-		autoCmd := &ishell.Cmd{
-			Name: "suggest",
-			Help: "try auto complete",
-			LongHelp: `Try dynamic autocomplete by adding and removing words.
-Then view the autocomplete by tabbing after "words" subcommand.
-This is an example of a long help.`,
-		}
-		autoCmd.AddCmd(&ishell.Cmd{
-			Name: "add",
-			Help: "add words to autocomplete",
-			Func: func(c *ishell.Context) {
-				if len(c.Args) == 0 {
-					c.Err(errors.New("missing word(s)"))
-					return
-				}
-				words = append(words, c.Args...)
-			},
-		})
-
-		autoCmd.AddCmd(&ishell.Cmd{
-			Name: "clear",
-			Help: "clear words in autocomplete",
-			Func: func(c *ishell.Context) {
-				words = nil
-			},
-		})
-
-		autoCmd.AddCmd(&ishell.Cmd{
-			Name: "words",
-			Help: "add words with 'suggest add', then tab after typing 'suggest words '",
-			Completer: func([]string) []string {
-				return words
-			},
-		})
-
-		shell.AddCmd(autoCmd)
-	}
-
-	shell.AddCmd(&ishell.Cmd{
-		Name: "paged",
-		Help: "show paged text",
-		Func: func(c *ishell.Context) {
-			lines := ""
-			line := `%d. This is a paged text input.
-This is another line of it.
-`
-			for i := 0; i < 100; i++ {
-				lines += fmt.Sprintf(line, i+1)
-			}
-			c.ShowPaged(lines)
-		},
-	})
-
-	cyan := color.New(color.FgCyan).SprintFunc()
-	yellow := color.New(color.FgYellow).SprintFunc()
-	boldRed := color.New(color.FgRed, color.Bold).SprintFunc()
-	shell.AddCmd(&ishell.Cmd{
-		Name: "color",
-		Help: "color print",
-		Func: func(c *ishell.Context) {
-			c.Print(cyan("cyan\n"))
-			c.Println(yellow("yellow"))
-			c.Printf("%s\n", boldRed("bold red"))
-		},
+		Name: "remove",
+		Help: "remove key",
+		Func: cacheClient.Remove(),
 	})
 
 	// when started with "exit" as first argument, assume non-interactive execution
